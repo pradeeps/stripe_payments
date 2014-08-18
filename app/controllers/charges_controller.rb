75f140753787b1ac27ae457e86949bbd591634bc
Jared Smith <jared.smith88@me.com>
2014-08-17 19:54:23 -0700

    add purchases model and controller

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -12,6 +12,15 @@ class ChargesController < ApplicationController
         :description => 'Growth Hacking Crash Course',
         :currency    => 'usd'
       )
+      purchase = Purchase.create(
+                  email: params[:stripeEmail], 
+                  card: params[:stripeToken], 
+                  amount: @amount, 
+                  description: charge.description, 
+                  currency: charge.currency,
+                  customer_id: customer.id, product_id: 1)
+      
+      redirect_to purchase
       rescue Stripe::CardError => e
         flash[:error] = e.message
         redirect_to charges_path
