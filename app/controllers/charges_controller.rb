0b23571893879c274992bbcebc8dfa1bd13bb1ff
Jared Smith <jared.smith88@me.com>
2014-12-26 21:27:34 -0800

    implemented subscription plan

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -1,23 +1,25 @@
 class ChargesController < ApplicationController
   def create
-      product = Product.find_by_sku('GROHACK1')
+      product = Product.find_by_sku('GROHACK2')
       customer = Stripe::Customer.create(
         :email => params[:stripeEmail],
-        :card  => params[:stripeToken]
+        :card  => params[:stripeToken],
+        :plan  => "GROWHACK2"
       )
 
-      charge = Stripe::Charge.create(
-        :customer    => customer.id,
-        :amount      => product.price_in_cents,
-        :description => product.full_description,
-        :currency    => 'usd'
-      )
+      # charge = Stripe::Charge.create(
+      #   :customer    => customer.id,
+      #   :amount      => product.price_in_cents,
+      #   :description => product.full_description,
+      #   :currency    => 'usd'
+      # )
+
       purchase = Purchase.create(
                   email: params[:stripeEmail], 
                   card: params[:stripeToken], 
                   amount: @amount, 
-                  description: charge.description, 
-                  currency: charge.currency,
+                  description: product.full_description, 
+                  currency: "usd",
                   customer_id: customer.id, 
                   product_id: product.id, 
                   uuid: SecureRandom.uuid)
